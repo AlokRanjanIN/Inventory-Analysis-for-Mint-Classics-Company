4023900e3c3a3b667b7676df8c02fa33a8450c70
AlokRanjanIN <131797730+AlokRanjanIN@users.noreply.github.com>
2023-08-07 23:27:03 +0530

    Update sql scripts.sql

--- sql scripts.sql
+++ sql scripts.sql
@@ -1,12 +1,49 @@
-#Question 1: Where are items stored and could a warehouse be eliminated?
+----------------------------------------------------------------------------------------------------------------------------------
+
+#Products currently in inventory.
+  
+SELECT productCode, productName, quantityInStock
+FROM products;
+
+----------------------------------------------------------------------------------------------------------------------------------
+
+#Determine important factors that may influence inventory reorganization/reduction.
+  
+SELECT p.productCode, p.productName, p.quantityInStock, p.productLine, p.buyPrice, p.MSRP,
+       w.warehouseName, w.warehousePctCap,
+       COUNT(od.orderNumber) AS totalOrders
+FROM products p
+JOIN warehouses w ON p.warehouseCode = w.warehouseCode
+JOIN productlines pl ON p.productLine = pl.productLine
+LEFT JOIN orderdetails od ON p.productCode = od.productCode
+GROUP BY p.productCode, p.productName, p.quantityInStock, p.productLine, p.buyPrice, p.MSRP,
+         w.warehouseName, w.warehousePctCap
+ORDER BY totalOrders DESC;
+
+----------------------------------------------------------------------------------------------------------------------------------
+
+#Where are items stored and could a warehouse be eliminated?
+  
 SELECT w.warehouseName, COUNT(p.productCode) AS ProductCount, SUM(p.quantityInStock) AS TotalQuantity
 FROM products p
 JOIN warehouses w ON p.warehouseCode = w.warehouseCode
 GROUP BY w.warehouseName
 ORDER BY ProductCount;
 
+----------------------------------------------------------------------------------------------------------------------------------
 
-#Question 2: How are inventory numbers related to sales figures? Are inventory counts appropriate?
+#Where are items stored and could a warehouse be eliminated?
+  
+SELECT w.warehouseName, COUNT(p.productCode) AS ProductCount, SUM(p.quantityInStock) AS TotalQuantity
+FROM products p
+JOIN warehouses w ON p.warehouseCode = w.warehouseCode
+GROUP BY w.warehouseName
+ORDER BY ProductCount;
+
+----------------------------------------------------------------------------------------------------------------------------------
+
+#How are inventory numbers related to sales figures? Are inventory counts appropriate?
+  
 SELECT p.productCode, p.productName, p.quantityInStock, SUM(od.quantityOrdered) AS TotalQuantityOrdered
 FROM products p
 JOIN orderdetails od ON p.productCode = od.productCode
@@ -14,8 +51,10 @@ JOIN orders o ON od.orderNumber = o.orderNumber
 GROUP BY p.productCode, p.productName, p.quantityInStock
 ORDER BY (p.quantityInStock - TotalQuantityOrdered);
 
+----------------------------------------------------------------------------------------------------------------------------------
 
-#Question 3: Are we storing non-moving items? Are any items candidates for being dropped?
+#Are we storing non-moving items? Are any items candidates for being dropped?
+  
 SELECT p.productCode, p.productName, p.quantityInStock, SUM(od.quantityOrdered) AS TotalQuantityOrdered
 FROM products p
 LEFT JOIN orderdetails od ON p.productCode = od.productCode
